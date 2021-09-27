17809d938e42de1c550aa1843979462e8be5510c
Michael-Van-Robbe <mittermayrm180114@sus.htl-grieskirchen.at>
2021-09-27 11:38:12 +0200

    Class structure for "Beispiel 3" was created

---
+++ src/net/htlgkr/mittermayrm180114/beispiel3/NumberTest.java
@@ -0,0 +1,6 @@
+package net.htlgkr.mittermayrm180114.beispiel3;
+
+public interface NumberTest
+{
+    boolean testNumber(int number);
+}
---
+++ src/net/htlgkr/mittermayrm180114/beispiel3/NumberTester.java
@@ -0,0 +1,39 @@
+package net.htlgkr.mittermayrm180114.beispiel3;
+
+public class NumberTester implements NumberTest
+{
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
+    public NumberTester(String fileName)
+    {
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester)
+    {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester)
+    {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester)
+    {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile()
+    {
+
+    }
+
+    @Override
+    public boolean testNumber(int number)
+    {
+        return false;
+    }
+}
